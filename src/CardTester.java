0a8128f396bf2bd18c4320316be19292577b9984
Ben Urbana <benurbana1@gmail.com>
2018-04-03 10:37:57 -0600

    Activity 1

--- src/CardTester.java
+++ src/CardTester.java
@@ -8,6 +8,20 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card card1 = new Card("King","Spades", 13);
+		Card card2 = new Card("Queen","Hearts", 12);
+		Card card3 = new Card("Jack","Clubs", 11);
+
+		System.out.println(card1.rank());
+		System.out.println(card2.rank());
+		System.out.println(card3.rank());
+
+		System.out.println(card1.pointValue());
+		System.out.println(card2.pointValue());
+		System.out.println(card3.pointValue());
+
+		System.out.println(card1.matches(card2));
+		System.out.println(card2.matches(card3));
+		System.out.println(card3.matches(card3));
 	}
 }
