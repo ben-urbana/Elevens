e44e310b120804ee4ed7d2c76bb3d55b1997c074
Ben Urbana <benurbana1@gmail.com>
2018-05-01 10:52:36 -0600

    Activity 4

---
+++ src/DeckTester.java
@@ -0,0 +1,22 @@
+/**
+ * This is a class that tests the Deck class
+ */
+public class DeckTester {
+
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		String[] ranks1 = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+		String[] suits1 = {"Spades", "Clubs", "Diamonds", "Hearts"};
+		int[] values1 = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+		Deck deck1 = new Deck(ranks1, suits1, values1);
+		deck1.shuffle(); 
+		
+		System.out.println(deck1.isEmpty());
+		System.out.println(deck1.size());
+		System.out.println(deck1.deal());
+	}
+}
+
